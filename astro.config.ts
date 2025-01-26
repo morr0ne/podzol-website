3386f42afa9a4a5b2c15f84e79d644551df5146b
Federico Maria Morrone <contact@morrone.dev>
2025-01-26 19:01:34 +0100

    Add tailwind and sitemap integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,14 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://podzol.morrone.dev",
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "load",
+  },
+  integrations: [sitemap()],
+  vite: { plugins: [tailwindcss()] },
+});
